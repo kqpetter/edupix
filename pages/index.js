e2bb183b70f225a8aabaa2a9acf9f92958fa5eac
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2021-08-08 00:06:40 +0200

    new frontpage

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://edupix.io">eduPix.io</a>
+           <a href="https://edupix.io">eduPix.io</a>
         </h1>
 
         <p className={styles.description}>
