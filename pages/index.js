453129dec4cf6ab1594e6cad3a64bf7750af3014
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2021-08-08 01:24:34 +0200

    New frontend

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-           <a href="https://edupix.io">eduPix.io</a>
+          Welcome to <a href="https://edupix.io">eduPix.io</a>
         </h1>
 
         <p className={styles.description}>
