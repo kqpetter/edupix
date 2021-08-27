040e314bc73c9bc7962ef355f647f814f181f9ab
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2021-08-27 16:29:32 +0200

    Search to mongoDB not json

--- pages/api/search/[title].js
+++ pages/api/search/[title].js
@@ -1,14 +1,26 @@
-import { articles } from '../../../images'
+//import { articles } from '../../../images'
+const MongoClient = require('mongodb').MongoClient;
 
+const uri = "mongodb+srv://christer:Ice278787@oerdev.bqfcd.mongodb.net/assessment?retryWrites=true&w=majority" //process.env.MONGODB_URI
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}
 export default function handler({ query: { title } }, res) {
-    let matches = articles.filter(article => {
-        const regex = new RegExp(`${title}`, 'gi');
-        return article.title.match(regex);
-    });
+    async function findResultsUi() {
 
-    if (matches) {
-        res.json(matches);
-    } else {
-        res.status(400).json({ msg: "No images not found" });
+
+        const client = new MongoClient(uri, options);
+        await client.connect();
+
+        const cursor = client.db("edupix").collection("collection").find({ title: { $regex: title } })
+        const results = await cursor.toArray();
+
+        res.json(results);
+        console.log(results);
+        return;
     }
+
+    findResultsUi().catch(console.error);
+
 }
